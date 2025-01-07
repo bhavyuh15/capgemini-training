2bb81c983d60276c12dc460a11200fe5dfb4521e
bhavyuh15 <bhavyadatey15@gmail.com>
2025-01-07 14:58:45 +0530

    multiplication function

--- capgemini training.java
+++ capgemini training.java
@@ -5,9 +5,13 @@ public class calculator{
 public static int add(int a,int b){
         return a+b;
      }
+public static int mult(int a,int b){
+        return a*b;
+
 public static void main(String args[]){
 System.out.print("Welcome to Calculator");
 int sum = sub(5,6);
 add(5,6);
-System.out.println(sum);
+mult(5,6);
+
 }}
