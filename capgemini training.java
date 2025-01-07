a6396c825c8e41dc9815b991851f79dd061670c8
bhavyuh15 <bhavyadatey15@gmail.com>
2025-01-07 15:04:14 +0530

    division function

--- capgemini training.java
+++ capgemini training.java
@@ -7,7 +7,10 @@ public static int add(int a,int b){
      }
 public static int mult(int a,int b){
         return a*b;
-
+}
+public static int div(int a,int b){
+        return a/b;
+}
 public static void main(String args[]){
 System.out.print("Welcome to Calculator");
 int sum = sub(5,6);
