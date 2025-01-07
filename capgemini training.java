bf07d805a142d277d6ce1c63382838ff29558ff9
bhavyuh15 <bhavyadatey15@gmail.com>
2025-01-07 13:51:04 +0530

    subttraction method added

---
+++ capgemini training.java
@@ -0,0 +1,9 @@
+public class calculator{
+ public static int sub(int a,int b){
+        return a-b;
+     }
+public static void main(String args[]){
+System.out.print("Welcome to Calculator");
+int sum = sub(5,6);
+System.out.println(sum);
+}}
